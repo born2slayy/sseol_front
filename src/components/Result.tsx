d636a822f5f28aa95c00c599e5f5f4712c033616
seonlim <dltjwl1@naver.com>
2024-09-01 21:00:10 +0900

    update: search

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,6 +1,6 @@
 import { ReactElement } from "react";
 import useInput, { InputState } from "../tools/useInput";
-import { Link, useLocation, useParams } from "react-router-dom";
+import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
 import { FaShoppingCart } from "react-icons/fa";
 import useContractAPI, { ContractApiOut } from "../api/useContractAPI";
 import { SearchApiResponse } from "../api/useSearchAPI";
@@ -8,12 +8,10 @@ import { SearchApiResponse } from "../api/useSearchAPI";
 //design: https://v0.dev/r/prIHE5fgaNv
 
 interface SearchBarProps {
-    searchState: InputState,
     solarOutput: string,
 }
 
 interface CardsProps {
-    search: string,
     data: CardProps[],
 }
 
@@ -57,16 +55,13 @@ function Card(props: CardProps): ReactElement {
     );
 }
 
-function Cards({ search, data }: CardsProps): ReactElement {
-    const pattern: RegExp = new RegExp(search, 'i');
-
+function Cards({ data }: CardsProps): ReactElement {
     return (
         <div 
             className="grid gap-6" 
             style={{ gridTemplateColumns: 'repeat(auto-fill, minmax(250px, 1fr))' }}
         >
             {data
-                .filter(cardData => pattern.test(cardData.brandName))
                 .map((cardData: CardProps, index: number): ReactElement => (
                     <Card key={index} {...cardData} />
                 ))}
@@ -74,12 +69,32 @@ function Cards({ search, data }: CardsProps): ReactElement {
     );
 }
 
-function SearchBar({ searchState, solarOutput }: SearchBarProps): ReactElement {
+function SearchBar({ solarOutput }: SearchBarProps): ReactElement {
+    const searchState: InputState = useInput("");
+    const navigate = useNavigate();
+    const handleKey = (e: React.KeyboardEvent<HTMLInputElement>): void => {
+        if (e.key === 'Enter') {
+            if (searchState.state.length == 0) {
+                alert("검색어를 입력해주세요.");
+                return;
+            }
+            navigate('/loading', {
+                state: {
+                    keyword: searchState.state,
+                    location: "",
+                    revenue: "",
+                    price: "",
+                    categories: ""
+                }
+            });
+        }
+    }
     return (
         <div className="mb-4">
             <input
                 placeholder="Search for brands..."
                 onChange={searchState.handler}
+                onKeyDown={handleKey}
                 className="w-full p-4 mb-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
             <p className="text-lg font-normal text-gray-400">
@@ -90,15 +105,14 @@ function SearchBar({ searchState, solarOutput }: SearchBarProps): ReactElement {
 }
 
 function Result(): ReactElement {
-    const searchState: InputState = useInput("");
     const location = useLocation();
     const data: SearchApiResponse = location.state!;
 
     return (
         <div className="mt-10 mb-10 ml-20 mr-20 p-8">
             <h1 className="text-4xl font-bold text-gray-900 mb-6">Fashion Brand Search</h1>
-            <SearchBar searchState={searchState} solarOutput={data.solarOutput} />
-            <Cards search={searchState.state} data={data.brands} />
+            <SearchBar solarOutput={data.solarOutput} />
+            <Cards data={data.brands} />
         </div>
     );
 }
