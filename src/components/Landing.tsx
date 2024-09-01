2739a73a1130e2b5b76b7c68c598998235cad58e
seonlim <dltjwl1@naver.com>
2024-09-01 14:54:15 +0900

    fix: ratio

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -29,7 +29,7 @@ function SelectBar(): ReactElement {
 function SearchSection(): ReactElement {
     return (
         <section className="w-full py-12 md:py-24 lg:py-32">
-            <div className="container px-4 md:px-6">
+            <div className="container mx-auto px-4 md:px-6">
                 <div className="flex flex-col items-center justify-center space-y-4 text-center">
                     <div className="space-y-2">
                         <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">What are you looking for?</h2>
